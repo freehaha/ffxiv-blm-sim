64146bf81340f1f1941291706d7a01b225c06840
Freehaha <freehaha@gmail.com>
2017-08-25 02:01:40 +1000

    implement mp and recast time

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-var skills = require('./skills.json');
+var skills = require('./skills');
 var config = require('./config.json');
 
+var state = {
+  phase: "FIRE",
+  stack: 3,
+};
 var next = function(state) {
 };
 
+// while(1) {
+//   var n = next();
+//   if(!n) {
+//     process.exit(0);
+//   }
+// }
+//
+
+var f = skills['Fire III'](state);
+console.log(f);
--- skills.js
+++ skills.js
@@ -8,14 +8,25 @@ for(var s of skills) {
     var skill = {};
     skill.name = s.name;
     skill.cast = s.cast;
+    skill.mp = s.mp;
     if(state.phase == "ICE") {
-      if(skill.type == "Fire") {
+      console.log(state.phase);
+      if(s.type == "FIRE") {
         skill.mp = config.UICostFire[state.stack - 1] * s.mp;
         skill.cast = config.UICastBonus[state.stack - 1] * s.cast;
       } else {
-        skill.cost = s.mp;
+        skill.mp = s.mp;
+      }
+    } else if(state.phase == "FIRE") {
+      console.log(state.phase);
+      if(s.type == "ICE") {
+        skill.mp = config.AFCostIce[state.stack - 1] * s.mp;
+        skill.cast = config.AFCastBonus[state.stack - 1] * s.cast;
+      } else if (s.type == "FIRE") {
+        skill.mp = config.AFCostFire[state.stack - 1] * s.mp;
       }
     }
+    return skill;
   };
 }
 
