3ce8f2406995b8204141bbcf12cb07d1a351a950
John Sun <freehaha@gmail.com>
2017-08-29 17:43:43 +1000

    implement mana tick and potency

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ var INIT_TICK = Math.floor(Math.random()*100 + 1)/100 * 3;
 console.log('tick ', INIT_TICK);
 
 var state = {
-  phase: "FIRE",
+  phase: "NONE",
   init: true,
   stack: 0,
   phaseTimer: 0,
+  time: 0,
+  recast: 0,
   tick: 0 - INIT_TICK,
   mp: config.MaxMp,
+  potency: 0,
 };
 
 function cast(state, spell) {
@@ -20,25 +23,77 @@ function cast(state, spell) {
     return false;
   }
   state.mp -= spell.mp;
+  state.potency += spell.potency;
   phase(state, spell);
-  tick(state);
+  state.recast = Math.max(spell.cast, config.gcd);
 }
 
 function tick(state) {
+  if(state.phaseTimer > 0) {
+    state.phaseTimer -= 0.01;
+  }
+  state.time += 0.01;
+  state.tick += 0.01;
+  state.recast -= 0.01;
+  if(state.recast < 0) {
+    state.recast = 0;
+  }
+  if(state.tick < 3) {
+    return;
+  }
+  console.log('tick!');
+  state.tick = 0;
+  // restore mp
+  if(state.stack < 0) {
+    var mpgain = Math.floor(config.MaxMp * config.UIMPBonus[Math.abs(state.stack) - 1]);
+    console.log('mp s:', state.stack, 'gain: ', mpgain);
+    state.mp += mpgain;
+  } else if(state.stack == 0) {
+    state.mp += config.MaxMp * 0.03;
+  }
+  if(state.mp > config.MaxMp) {
+    state.mp = config.MaxMp;
+  }
 }
 
 function phase(state, spell) {
+  // console.log('phase', state, spell);
   if(spell.name === 'Fire III') {
-    state.phase = 'FIRE';
     state.stack = 3;
+    state.phaseTimer = config.PhaseDuration;
   } else if(spell.name === 'Blizzard III') {
-    state.phase = 'ICE';
+    state.stack = -3;
+    state.phaseTimer = config.PhaseDuration;
+  } else if(state.stack == 0) {
+    state.stack += (spell.type==='FIRE'?1:-1);
+  } else if(state.stack > 0 && spell.type === 'FIRE') {
+    state.phaseTimer = config.PhaseDuration;
+    state.stack++;
+  } else if(state.stack < 0 && spell.type === 'ICE') {
+    state.phaseTimer = config.PhaseDuration;
+    state.stack--;
+  } else {
+    console.log('reset 0');
+    state.phaseTimer = 0;
+    state.stack = 0;
+  }
+
+  if(state.stack > 3) {
+    state.stack = 3;
+  } else if(state.stack < -3) {
     state.stack = -3;
   }
-  console.log(state);
+  if(state.stack > 0) {
+    state.phase = "FIRE";
+  } else if(state.stack < 0) {
+    state.phase = 'ICE';
+  }
 }
 
 var next = function(state) {
+  if(state.recast > 0) {
+    return 1;
+  }
   if(state.init) {
     var s = skills['Fire III'](state);
     cast(state, s);
@@ -46,16 +101,36 @@ var next = function(state) {
     return;
   }
   var stack = state.stack;
-  if(state.phase === 'FIRE') {
-    var s = skills['Fire'](state);
-    cast(state, s);
+  if(state.stack > 0) {
+    var f1 = skills['Fire'](state);
+    var b3 = skills['Blizzard III'](state);
+    if(f1.mp + b3.mp < state.mp) {
+      cast(state, f1);
+    } else {
+      cast(state, b3);
+    }
+  } else if (state.stack < 0) {
+    var b1 = skills['Blizzard'](state);
+    var f3 = skills['Fire III'](state);
+    if(state.mp < b1.mp) {
+      return 1;
+    }
+    if(state.mp < config.MaxMp) {
+      cast(state, b1);
+    } else {
+      cast(state, f3);
+    }
+  } else {
+    console.log(state);
+    // var b3 = skills['Blizzard III'](state);
+    // cast(state, b3);
   }
   return 1;
 };
 
-for(var i = 0; i < 100; ++i) {
+for(var i = 0; i < 600*100; ++i) {
+  tick(state);
   var n = next(state);
-  if(!n) {
-    process.exit(0);
-  }
 }
+console.log("potency", state.potency);
+console.log("pps", state.potency/state.time);
--- skills.js
+++ skills.js
@@ -4,32 +4,39 @@ var config = require('./config.json');
 var skillFuncs = {};
 
 for(var s of skills) {
-  skillFuncs[s.name] = function(state) {
-    var skill = {};
-    skill.name = s.name;
-    skill.cast = s.cast;
-    skill.mp = s.mp;
-    var stack = Math.abs(state.stack);
-    if(stack == 0) {
+  (function(s) {
+    skillFuncs[s.name] = function(state) {
+      var skill = {};
+      skill.name = s.name;
+      skill.cast = s.cast;
+      skill.potency = s.potency;
+      skill.mp = s.mp;
+      skill.type = s.type;
+      var stack = Math.abs(state.stack);
+      if(stack == 0) {
+        return skill;
+      }
+      if(state.phase == "ICE") {
+        if(s.type == "FIRE") {
+          skill.mp = config.UICostFire[stack - 1] * s.mp;
+          skill.cast = config.UICastBonus[stack - 1] * s.cast;
+          skill.potency = config.UIPenalty[stack - 1] * s.potency;
+        } else {
+          skill.mp = s.mp;
+        }
+      } else if(state.phase == "FIRE") {
+        if(s.type == "ICE") {
+          skill.mp = config.AFCostIce[stack - 1] * s.mp;
+          skill.cast = config.AFCastBonus[stack - 1] * s.cast;
+          skill.potency = config.AFPenalty[stack - 1] * s.potency;
+        } else if (s.type == "FIRE") {
+          skill.mp = config.AFCostFire[stack - 1] * s.mp;
+          skill.potency = config.AFBonus[stack - 1] * s.potency;
+        }
+      }
       return skill;
-    }
-    if(state.phase == "ICE") {
-      if(s.type == "FIRE") {
-        skill.mp = config.UICostFire[stack - 1] * s.mp;
-        skill.cast = config.UICastBonus[stack - 1] * s.cast;
-      } else {
-        skill.mp = s.mp;
-      }
-    } else if(state.phase == "FIRE") {
-      if(s.type == "ICE") {
-        skill.mp = config.AFCostIce[stack - 1] * s.mp;
-        skill.cast = config.AFCastBonus[stack - 1] * s.cast;
-      } else if (s.type == "FIRE") {
-        skill.mp = config.AFCostFire[stack - 1] * s.mp;
-      }
-    }
-    return skill;
-  };
+    };
+  })(s);
 }
 
 module.exports = skillFuncs;
